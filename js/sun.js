29263734f211aa6d68053332efc4d81b4da6ab3b
Eva <evariksen@gmail.com>
2018-03-19 15:52:07 +0100

    sun path fix

--- js/sun.js
+++ js/sun.js
@@ -1,7 +1,7 @@
 var sun = document.getElementsByClassName('sun')[0];
 var path = new TimelineMax({repeat: -1});
   
-path.to(sun, 38.8, 
+path.to(sun, 38.0, 
 {bezier:{type:'quadratic', 
 values:[
 {x:0, y:0},{x:650, y:0},{x:650, y:650},  
